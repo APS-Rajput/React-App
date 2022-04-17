6ea748e0c0316383c798a02fed5bb8eefb7f9616
Arun <arunrajput.2603@gmail.com>
2022-04-17 15:31:34 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import Users_card from "./Components/users";
 import './App.css';
 
 import React, { Component } from 'react'
-	
-class App extends Component {
-  constructor(props){
-	super(props)
-		
-	// Set initial state
-	this.state = {users_data :[]
-  }  
 
-	this.updateState = this.updateState.bind(this)
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    // Set initial state
+    this.state = {
+      users_data: []
+    }
+
+    this.updateState = this.updateState.bind(this)
   }
-    
-  updateState(){
-      const link="https://reqres.in/api/users?page=1";
-      fetch(link)
+
+  updateState() {
+    const link = "https://reqres.in/api/users?page=1";
+    fetch(link)
       .then(response => response.json())
       .then((users) => {
-        
-        this.setState({users_data :users.data
+
+        this.setState({
+          users_data: users.data
         })
         console.log(users.data);
       })
@@ -28,23 +30,23 @@ class App extends Component {
         console.error(error)
       })
   }
-    
-  render(){
+
+  render() {
     return (<>
       <nav>
-          <label className="logo">Coding Cavalry</label>
-            <ul>
-              <li><button onClick={this.updateState} className="button-56">Get Users</button> 
-            </li>
-            </ul>
-            
-        </nav>
-        <div className="box">
-         <Users_card users={this.state.users_data}/>
-         </div>
+        <label className="logo">Coding Cavalry</label>
+        <ul>
+          <li><button onClick={this.updateState} className="button-56">Get Users</button>
+          </li>
+        </ul>
+
+      </nav>
+      <div className="box">
+        <Users_card users={this.state.users_data} />
+      </div>
     </>
     )
   }
 }
-	
+
 export default App;
--- src/Components/users.js
+++ src/Components/users.js
@@ -1,20 +1,20 @@
 import React from 'react'
 
-const Users_card = ({users}) => {
-    return users.map(user =>
-      <div className="project column-3">
-        {
-          console.log(user)
-        }
-        <div className="profile">
-          <img src={user.avatar} alt={user.avatar} className="avatar"></img>
-          <h1 className="name">{user.first_name} {user.last_name}</h1>
-          <p className="userid">User-id : {user.id}</p>
-          <p className="email">{user.email}</p>
-          
-        </div>
+const Users_card = ({ users }) => {
+  return users.map(user =>
+    <div className="project column-3">
+      {
+        console.log(user)
+      }
+      <div className="profile">
+        <img src={user.avatar} alt={user.avatar} className="avatar"></img>
+        <h1 className="name">{user.first_name} {user.last_name}</h1>
+        <p className="userid">User-id : {user.id}</p>
+        <p className="email">{user.email}</p>
+
       </div>
-)
+    </div>
+  )
 }
 
 export default Users_card;
